692740f1e1c40d8d95536992d49f544e3aaab2fb
ObitoUtiha <82911832+ObitoUtiha@users.noreply.github.com>
2025-11-25 18:07:58 +0300

    Add main project

---
+++ src/NorbitPizzaApp/NorbitPizzaApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NorbitPizzaApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
